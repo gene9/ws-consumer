49ecf7970a645ec5fe4552dff8c01bedb84db728
gene9 <gene9@example.com>
2014-12-29 11:47:26 -0600

    load raw data from postgresql

--- src/main/scala/com/lightdruid/examples/ws_consumer/WsConsumer.scala
+++ src/main/scala/com/lightdruid/examples/ws_consumer/WsConsumer.scala
@@ -1,5 +1,15 @@
 package com.lightdruid.examples.ws_consumer
 
+import java.sql.DriverManager
+
+import org.jooq.SQLDialect
+import org.jooq.impl.{DSL}
+
 object WsConsumer extends App {
   println("test")
+
+  val c = DriverManager.getConnection("jdbc:postgresql:test", "test", "test");
+  val e = DSL.using(c, SQLDialect.POSTGRES);
+
+  println(e.fetch("select id from white_list"))
 }
