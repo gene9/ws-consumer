ecbc466a0c35b8a9520b1b69cdc2b8520e753600
gene9 <gene9@example.com>
2014-12-29 12:22:40 -0600

    added simple use cases

--- src/main/scala/com/lightdruid/examples/ws_consumer/WsConsumer.scala
+++ src/main/scala/com/lightdruid/examples/ws_consumer/WsConsumer.scala
@@ -1,9 +1,12 @@
 package com.lightdruid.examples.ws_consumer
 
-import java.sql.DriverManager
+import java.sql.{Timestamp, DriverManager}
 
+import org.joda.time.DateTime
 import org.jooq.SQLDialect
 import org.jooq.impl.{DSL}
+
+import com.lightdruid.examples.generated.tables._
 
 object WsConsumer extends App {
   println("test")
@@ -12,4 +15,13 @@ object WsConsumer extends App {
   val e = DSL.using(c, SQLDialect.POSTGRES);
 
   println(e.fetch("select id from white_list"))
+  
+  val now = new Timestamp(DateTime.now.getMillis())
+  val rec = e.select(WhiteList.WHITE_LIST.ID, WhiteList.WHITE_LIST.IP)
+    .from(WhiteList.WHITE_LIST)
+    .where(WhiteList.WHITE_LIST.CREATED_AT.gt(now))
+    .fetchOne()
+
+  println(rec.getValue("ip"))
+  println(rec.value2())
 }
